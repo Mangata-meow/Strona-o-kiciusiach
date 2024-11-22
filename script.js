61d1c0a5d8762d63f7d39965b761dbc66bd4b31a
Julia Mrowiec <148622333+Mangata-meow@users.noreply.github.com>
2024-11-22 11:24:36 +0000

    Animacja ładowania strony

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 
 document.getElementById("current-date").textContent = "Bieżąca data: " + new Date().toLocaleDateString('pl-PL');
+window.onload = function() {
+    document.body.classList.add("loaded"); // Po załadowaniu strony, uruchom animację
+};
